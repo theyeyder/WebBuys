c8b46a7c30ff891cd3bff086e3f9e421d1f3a65c
heyeyder <eyderarroyo19@gmail.com>
2026-07-16 13:53:34 -0500

    Config user

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,17 +1,65 @@
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import Usuario from '../models/Usuario.js';
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import Usuario from "../models/Usuario.js";
 
 export async function login(req, res) {
   try {
     const { usuario, password } = req.body;
-    const user = await Usuario.findOne({ usuario: usuario?.toLowerCase(), estado: true });
-    if (!user) return res.status(401).json({ message: 'Usuario o contraseña incorrectos' });
-    const valid = await bcrypt.compare(password, user.password);
-    if (!valid) return res.status(401).json({ message: 'Usuario o contraseña incorrectos' });
-    const token = jwt.sign({ id: user._id, rol: user.rol }, process.env.JWT_SECRET, { expiresIn: '8h' });
-    res.json({ token, user: { id: user._id, nombre: user.nombre, usuario: user.usuario, rol: user.rol } });
+
+    const user = await Usuario.findOne({
+      usuario: usuario.toLowerCase(),
+    }).select("+password");
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Usuario o contraseña incorrectos",
+      });
+    }
+
+    if (user.estado !== "Activo") {
+      return res.status(401).json({
+        message: "El usuario se encuentra bloqueado.",
+      });
+    }
+
+    const valido = await bcrypt.compare(password, user.password);
+
+    if (!valido) {
+      return res.status(401).json({
+        message: "Usuario o contraseña incorrectos",
+      });
+    }
+
+    user.ultimoIngreso = new Date();
+    await user.save();
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        rol: user.rol,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "8h",
+      }
+    );
+
+    res.json({
+      token,
+
+      user: {
+        id: user._id,
+        nombres: user.nombres,
+        apellidos: user.apellidos,
+        usuario: user.usuario,
+        rol: user.rol,
+        estado: user.estado,
+        debeCambiarPassword: user.debeCambiarPassword,
+      },
+    });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({
+      message: error.message,
+    });
   }
 }
